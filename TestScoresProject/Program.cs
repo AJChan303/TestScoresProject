9d13d26d7f2d593d030a7b950c005ea0c5158772
Alex Chan <AJChan303@gmail.com>
2019-07-25 09:38:09 -0400

    Creating Program to average test scores

--- TestScoresProject/Program.cs
+++ TestScoresProject/Program.cs
@@ -3,7 +3,17 @@
 namespace TestScoresProject {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            int[] scores = new int[12] { 71, 78, 89, 73, 68, 73, 96, 71, 72, 65, 87, 63 };
+            int avg = 0;
+            int total = 0;
+            int index = 0;
+            while (index < 12) {
+                total = total + scores[index];
+                index = index + 1;
+                avg = total / 12;
+
+            }
+            Console.WriteLine($"The Average is {avg} + %");
         }
     }
 }
